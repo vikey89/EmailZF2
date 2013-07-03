488334f63c50a951ea70ead5cd8cb1914143fa42
Vincenzo Provenza <vichi89@hotmail.it>
2013-07-03 16:12:28 +0200

    Update EmailZF2

--- config/module.config.php
+++ config/module.config.php
@@ -1,20 +1,4 @@
 <?php
 return array(
-    'email' => array(
-        'credentials' => array(
-            'from_mail' => 'support@domain.com',
-            'from_name' => 'yourname',
-        ),
-        'transport' => 'sendmail', //use smtp or sendmail
-        'smtp' => array(
-            'host' => 'smtp.domain.com',
-            'port' => 587,
-            'connection_class' => 'login',
-            'connection_config' => array(
-                'ssl'      => 'tls',
-                'username' => 'yourgmailusername',
-                'password' => 'yourgmailpassword'
-            ),
-        ),
-    ),
+
 );
